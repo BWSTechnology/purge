9b50dc648fa31e47109dac01c4ee7b59c60eace7
BWSTechnology <bwstechnology@gmail.com>
2014-03-08 12:25:58 +0100

    Basic technological change: using ScrollView/LinearLayout instead of ListView.
    Advancement: made it possible to massivly reduce complexity of code and
    enable features
    
    Features: infinite length of list possible, load + save + append data

--- purge/src/com/bwstechnology/purge/MainActivity.java
+++ purge/src/com/bwstechnology/purge/MainActivity.java
@@ -1,21 +1,34 @@
 package com.bwstechnology.purge;
- 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.widget.ListView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
  
-public class MainActivity extends Activity implements OnGestureListener{
-    private ListView toDoList;
-    private TextAdapter myAdapter;
+public class MainActivity extends Activity implements OnGestureListener, TextWatcher{
+    private LinearLayout toDoList;
+    //private TextAdapter myAdapter;
     private GestureDetector mGestureDetector;
+    private int position = 0;
  
     /** Called when the activity is first created. */
     @Override
@@ -24,11 +37,16 @@ public class MainActivity extends Activity implements OnGestureListener{
     	super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
 	 
-	    // get list view, set some properties and bind data source to it
-	    toDoList = (ListView) findViewById(R.id.ToDoListView);
-	    toDoList.setItemsCanFocus(true);
-	    myAdapter = new TextAdapter(this);
-	    toDoList.setAdapter(myAdapter);
+	    if (!this.loadData())
+	    {
+		    // get list view, set some properties and bind data source to it
+		    toDoList = (LinearLayout) findViewById(R.id.linearLayout);
+		    
+		    EditText text1 = new EditText(this);
+		    text1.addTextChangedListener(this);
+		    
+		    toDoList.addView(text1);
+	    }
 	    
 	    mGestureDetector = new GestureDetector(this, this);
     }
@@ -39,6 +57,139 @@ public class MainActivity extends Activity implements OnGestureListener{
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    
+    // start TextWatcher
+    
+    public void afterTextChanged(Editable s)
+    {
+    	
+    }
+    
+    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+    {
+    	if (this.isExternalStorageWritable())
+    	{
+	    	LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+	    	EditText edit = (EditText)linearLayout.getFocusedChild();
+	    	position = linearLayout.indexOfChild(edit);
+	        
+	        if (position == (linearLayout.getChildCount()-1))
+	        {   
+	        	EditText text1 = new EditText(this);
+	    	    text1.addTextChangedListener(this);
+	    	    
+	    	    //toDoList.addView(text1);
+	    	    linearLayout.addView(text1);
+	        }
+    	}
+    }
+    
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+    
+    public View getView(int position, View convertView, ViewGroup parent) {
+    	this.position = position;
+    	return convertView;
+    }
+    
+    // end TextWatcher
+    
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+        	writeData();
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean loadData()
+    {
+    	boolean fileLoaded = false;
+    	File directory = Environment.getExternalStorageDirectory();
+		try
+		{
+			File dir = new File(directory.getAbsolutePath() + "/download");
+	        dir.mkdirs();
+	        File file = new File(dir, "myData.txt");
+	        try {
+	        	BufferedReader br = new BufferedReader(new FileReader(file));
+	            String line;
+	            
+	            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+	            
+	            while ((line = br.readLine()) != null) {
+	            	EditText text1 = new EditText(this);
+	            	text1.setText(line);
+	        	    text1.addTextChangedListener(this);
+	        	    
+	        	    linearLayout.addView(text1);
+	            }
+	            
+	            /*for (int i = 0; i < linearLayout.getChildCount(); i++)
+	            {
+	            	
+	                //this.listItemFragment.editTextField = (EditText) convertView.findViewById(R.id.ContentField);
+	            }*/
+	            br.close();
+	            fileLoaded = true;
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+		catch(Exception exp)
+		{
+		}
+		return fileLoaded;
+    }
+
+    
+    private void writeData()
+    {
+    		File directory = Environment.getExternalStorageDirectory();
+    		try
+    		{
+    			File dir = new File(directory.getAbsolutePath() + "/download");
+    	        dir.mkdirs();
+    	        File file = new File(dir, "myData.txt");
+    	        try {
+    	            FileOutputStream f = new FileOutputStream(file);
+    	            PrintWriter pw = new PrintWriter(f);
+    	            
+    	            // get listview view
+    	            // for each edittext, get text
+    	            
+    	            LinearLayout linerLayout = (LinearLayout) findViewById(R.id.linearLayout);
+    	            for (int i = 0; i < linerLayout.getChildCount(); i++)
+    	            {
+    	            	EditText editText = (EditText) linerLayout.getChildAt(i);
+    	            	//editText.getText();
+    	            	// write value
+    		    		pw.println(editText.getText().toString());
+    	            }
+    	            
+    	            /*
+    		    	for (int i = 0; i < arrayListListItems.size(); i++)
+    	            {
+    		    		// write value
+    		    		pw.println(arrayListListItems.get(i).editTextField.getText().toString());
+    		    	}
+    		    	*/
+    	            pw.flush();
+    	            pw.close();
+    	            f.close();
+    	        } catch (FileNotFoundException e) {
+    	            e.printStackTrace();
+    	        } catch (IOException e) {
+    	            e.printStackTrace();
+    	        }
+    		}
+    		catch(Exception exp)
+    		{
+    		}
     }
     
     public boolean onOptionsItemSelected(MenuItem item)
@@ -71,7 +222,7 @@ public class MainActivity extends Activity implements OnGestureListener{
 		if(e2.getX() - e1.getX() > sensitivity)
 		{
 			//remove edittext from view
-			ListView listView = (ListView) findViewById(R.id.ToDoListView);
+			//ListView listView = (ListView) findViewById(R.id.ToDoListView);
 			
 			return true;
 		}
--- purge/src/com/bwstechnology/purge/TextFieldContainer.java
+++
@@ -1,7 +0,0 @@
-package com.bwstechnology.purge;
-
-import android.widget.EditText;
-
-class TextFieldContainer {
-    EditText editTextField;
-}
